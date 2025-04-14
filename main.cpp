0c108ad709eb8dae3eca529f81274f5d4491efb9
heefee <davidpeter11356@gmail.com>
2025-04-14 20:55:49 +0300

    added input file stream

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "MediaItem.h"
 #include "Repository.h"
+#include <fstream>
+
 using namespace std;
 
 int main() {
     Repository r;
+    ifstream f{"../media_entries.csv"};
+    cout<<f.is_open();
     return 0;
 }
